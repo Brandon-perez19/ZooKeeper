1bab956d3bb33d3679dab8705b2eda3d9d940ceb
Brandon <brandonperez@brandons-mbp.lan>
2022-05-19 23:19:08 -0400

    coded port to be a variable instead of hardcoded

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 //assign expresss to app variable to chain methods later
 const app = express();
 
@@ -50,7 +51,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 
 });
